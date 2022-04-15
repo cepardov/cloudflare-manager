eded20728fca01767e139be9b97d232339ab119a
Cristian Pardo <cristian.pardo@externos.bci.cl>
2022-04-15 12:37:58 -0400

    Initial commit

---
+++ src/main/java/cl/cepardov/cloudflare/CloudflareApplication.java
@@ -0,0 +1,13 @@
+package cl.cepardov.cloudflare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudflareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudflareApplication.class, args);
+    }
+
+}
